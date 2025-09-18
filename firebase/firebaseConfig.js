e9b1f045a23fa1e81a7b120f839e6543a1a67792
nicogit20 <nicogit20@gmail.com>
2025-09-18 09:40:25 +0530

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -9,12 +9,12 @@ import { Platform } from "react-native";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPbEycFiFRaZGeoheIKHtK0p36j31FPAI",
-  authDomain: "fir-demo-ea5ae.firebaseapp.com",
-  projectId: "fir-demo-ea5ae",
-  storageBucket: "fir-demo-ea5ae.appspot.com",
-  messagingSenderId: "209228824130",
-  appId: "1:209228824130:web:2b8071d6fc74bb9ffbb7db",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
